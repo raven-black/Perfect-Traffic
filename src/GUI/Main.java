2a8427fabf1d233c40855d9fad3b335bf549e4b2
raven-black <Remo@Remo_Laptop.fritz.box>
2013-07-08 10:11:12 +0200

    erstes Hochladen von Remo: Autoknoten,Straßenknoten, Objektliste,Überprüfung von Variablen(nicht vollständig)

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -9,6 +9,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.*;
 import javax.swing.JPanel;
+import classes.*;
 
 /**
  *
@@ -23,6 +24,8 @@ public class Main extends javax.swing.JFrame implements MouseListener {
     private String object;
     public int ix,iy,ax,ay;
     IDraw paint;
+    private Objektliste Auto;
+    private Objektliste Straße;
     
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -193,6 +196,82 @@ public class Main extends javax.swing.JFrame implements MouseListener {
     private void b_addBridgeMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_b_addBridgeMouseClicked
         // TODO add your handling code here: PLACE BRIDGE
     }//GEN-LAST:event_b_addBridgeMouseClicked
+    
+    private void ueberpruefung(){  //Hier werden die Variablen Straßenauslastung,Autos auf Straße, zugehörigkeit,stehende Autos überprüft und aktualisiert
+    
+    
+        if(Auto.getAnfangStraße()==null||Straße.getAnfangAuto()==null){}
+        else{
+            Autoknoten aktuellAuto=Auto.getAnfangAuto();            //Definition der Variable in der Autoliste
+            Straßenknoten aktuellStraße=Straße.getAnfangStraße();   //Definition der Variable in der Straßenliste
+        
+        
+            //Zugehoerigkeit:                                                                                                    //Diese Formeln beachten noch nicht, dass die Autos nicht in der Mitte der Fahrbahn, sondern in der Mitte eines Fahrstreifen fahren.
+        
+            double r,s;boolean ueberpruefung=false;
+            do{
+                r=Math.round(((aktuellAuto.getx()-aktuellStraße.getx())/(aktuellStraße.getx2()-aktuellStraße.getx()))*100)/100; //Mit hilfe der Verktorrechnung den Wert fur r, mit den man den Richtungsvektor der Straße bestimmt.
+                s=Math.round(((aktuellAuto.gety()-aktuellStraße.gety())/(aktuellStraße.gety2()-aktuellStraße.gety()))*100)/100; //Mit hilfe der Verktorrechnung den Wert fur s, mit den man den Richtungsvektor der Straße bestimmt.
+                if(s==r)
+                {
+                    aktuellAuto.setzugehoerigkeit(aktuellStraße.getindentifikation());  //Wenn r und s gleich sind ist das Auto auf der Straße
+                }
+            }
+            while(aktuellStraße.getNF()!=null||ueberpruefung==false);   
+            ueberpruefung=false;
+            aktuellStraße=Straße.getAnfangStraße();
+            do{                                                                 //Hiermit wird das für jedes Auto auf jeder Straße wiederholt 
+                aktuellAuto=aktuellAuto.getNF();
+                    do{                                                         //Hier wird das gleiche für jede Straße gemacht.
+                    aktuellStraße=aktuellStraße.getNF();
+                    r=Math.round(((aktuellAuto.getx()-aktuellStraße.getx())/(aktuellStraße.getx2()-aktuellStraße.getx()))*100)/100;
+                    s=Math.round(((aktuellAuto.gety()-aktuellStraße.gety())/(aktuellStraße.gety2()-aktuellStraße.gety()))*100)/100;
+                    if(s==r){aktuellAuto.setzugehoerigkeit(aktuellStraße.getindentifikation());ueberpruefung=true;} 
+                    }
+                    while(aktuellStraße.getNF()!=null||ueberpruefung==false);   //Die Anweisung wird abgebrochen sobald es einen Traffer gibt,da nur eines möglich ist.
+                ueberpruefung=false;
+                aktuellStraße=Straße.getAnfangStraße();
+            }
+            while(aktuellAuto.getNF()!=null);
+            
+            
+            //Stehende Autos, Autos auf Straße und Straßenauslastung:
+            
+            int[] zugehoerigkeit=new int[Straße.getAnzahlStraßen()];                                                                                                    ////////////Noch zu beachten: Identifikation muss bei 0 anfangen, da dies das Array auch tut
+            int[] stehend=new int[Straße.getAnzahlStraßen()];               //Diese 2 Arrays sind zählen die der stehenden Autos für jede Straße(i und j)
+            int i,j;
+            
+            aktuellAuto=Auto.getAnfangAuto();
+            while(aktuellAuto.getNF()!=null){                   //die sorgt dafür, das es mit allen Autos gemacht wird
+                i=aktuellAuto.getzugehoerigkeit();              //Hier wird i als die Straße des Autos definiert
+                if(aktuellAuto.getkmh()==0){
+                    j=aktuellAuto.getzugehoerigkeit();//Hier wird das gleiche gemacht mit den Autos, die 0kmh fahren
+                    stehend[j]=stehend[j]+1;//Dies wird jetzt in den Array gespeichert
+                } 
+                zugehoerigkeit[i]=zugehoerigkeit[i]+1;  //""
+                aktuellAuto=aktuellAuto.getNF();
+            }
+        
+            i=aktuellAuto.getzugehoerigkeit();                              //Das gleiche nochmal, weil dies in der Schleife nicht mit inbegriffen ist.
+            if(aktuellAuto.getkmh()==0){
+                j=aktuellAuto.getzugehoerigkeit();
+                stehend[j]=stehend[j]+1;
+            }
+            zugehoerigkeit[i]=zugehoerigkeit[i]+1;
+            
+            while(aktuellStraße.getNF()!=null){                     //Hier werden die Informationen in der Liste gespeichert
+                aktuellStraße.setautos_auf_straße(zugehoerigkeit[aktuellStraße.getindentifikation()]);
+                aktuellStraße.setstehende_Autos(stehend[aktuellStraße.getindentifikation()]);
+                aktuellStraße.setstraßenauslastung();               //Die Straßenauslastung wird aus den Autos und der Länge der Straße ausgerechnet 
+                aktuellStraße=aktuellStraße.getNF();
+            }
+            aktuellStraße.setautos_auf_straße(zugehoerigkeit[aktuellStraße.getindentifikation()]);   //Wurde in der Schleife nicht inbegriffen
+            aktuellStraße.setstehende_Autos(stehend[aktuellStraße.getindentifikation()]);
+            aktuellStraße.setstraßenauslastung();
+        }   
+    
+    
+    }
     
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
